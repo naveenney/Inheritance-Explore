3391664c5e293d68378147c52c544602b4e48ec5
naveenney <159130020+naveenney@users.noreply.github.com>
2024-03-02 21:53:10 +0530

    Add files via upload

---
+++ Bag.java
@@ -0,0 +1,45 @@
+package Assign;
+
+public class Bag extends Box{
+
+	void colour()
+	{
+	  	System.out.println("Bag colour method is calling");
+	}
+	
+	void size()
+	{
+		System.out.println("Bag Size Method is Calling");
+	}
+	
+	void shape()
+	{
+		System.out.println("Bag Shape method calling");
+	}
+	
+	void state()
+	{
+		System.out.println("Bag State Method is calling");
+	}
+	
+	void store()
+	{
+		System.out.println("Bag store Method is calling");
+	}
+	
+	void clean()
+	{
+		System.out.println("Bag Clean method is calling");
+	}
+	void open()
+	{
+		System.out.println("Bag open method calling");
+	}
+	
+	void close()
+	{
+		System.out.println("Bag close method calling");
+	}
+
+	
+}
